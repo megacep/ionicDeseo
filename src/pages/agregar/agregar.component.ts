59830947dbfcea71ba78ba7104d11cafa3036bd7
Megacep <inflanter@hotmail.com>
2018-07-17 18:07:57 -0300

    Agregando items a la lista

--- src/pages/agregar/agregar.component.ts
+++ src/pages/agregar/agregar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Lista,ListaItem} from '../../app/clases/index'
+
 
 @Component({
     selector: 'app-agregar',
@@ -7,10 +9,32 @@ import { Component, OnInit } from '@angular/core';
 export class AgregarComponent implements OnInit {
     
     nombreLista:string;    
-    nombreItem:string; 
+    nombreItem:string = ""; 
+
+    items:ListaItem[] = [];
 
     constructor() {         
     }
 
-    ngOnInit() { }
+    ngOnInit() {         
+    }
+
+    agregar(){
+        
+        if( this.nombreItem.length == 0 ){
+            return;
+        }
+        else{
+
+            let item = new ListaItem();
+            item.nombre = this.nombreItem;
+            item.completado = false;
+
+            this.items.push(item);
+
+            this.nombreItem="";
+
+        }
+            
+    }
 }
