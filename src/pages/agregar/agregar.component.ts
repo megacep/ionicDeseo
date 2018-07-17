78a2f0c9e324319545855c29258e3029e927fe7b
Megacep <inflanter@hotmail.com>
2018-07-17 18:26:31 -0300

    Se termina la pantalla de agregar items

--- src/pages/agregar/agregar.component.ts
+++ src/pages/agregar/agregar.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import {Lista,ListaItem} from '../../app/clases/index'
+import { AlertController, NavController } from 'ionic-angular'
+
+import { ListaDeseosService } from '../../app/services/lista-deseo.service';
+
+
+
 
 
 @Component({
@@ -8,12 +14,14 @@ import {Lista,ListaItem} from '../../app/clases/index'
 })
 export class AgregarComponent implements OnInit {
     
-    nombreLista:string;    
+    nombreLista:string = "";    
     nombreItem:string = ""; 
 
     items:ListaItem[] = [];
 
-    constructor() {         
+    constructor( public alertCtrl: AlertController,
+                 public navCtrl:NavController,
+                 private _deseoService: ListaDeseosService) {         
     }
 
     ngOnInit() {         
@@ -31,10 +39,34 @@ export class AgregarComponent implements OnInit {
             item.completado = false;
 
             this.items.push(item);
-
             this.nombreItem="";
 
+        }            
+    }
+
+    borrarDeLista(index){
+        this.items.splice(index,1);  
+    }
+
+    guardarLista(){
+        if( this.nombreLista.length ==0){
+            let alert = this.alertCtrl.create({
+                title: "Nombre de la lista",
+                subTitle: "El nombre de la lista es necesario",
+                buttons: ['ok']
+            });
+            alert.present();
+            return;
         }
-            
+
+        let lista = new Lista(this.nombreLista)
+
+        lista.items = this.items;
+
+        this._deseoService.listas.push(lista);        
+
+        this.navCtrl.pop();
+
+
     }
 }
